e48f9eec74d2d6461197414fae3ac1c080382920
Sheilllah <154285621+Sheilllah@users.noreply.github.com>
2024-12-25 23:26:08 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Redirect after 1 minute (60,000 milliseconds)
+setTimeout(() => {
+    window.location.href = "https://lhinspire.blogspot.com"; // Replace with your desired URL
+}, 60000); // 60,000 milliseconds = 1 minute
